14256ad62da96ba8c5924fb53d98301a45ca12a2
01lcw <163083611+01lcw@users.noreply.github.com>
2025-07-17 16:41:21 +0900

    Add files via upload

---
+++ hk/edu20250717/day11/D1_CustomerMain.java
@@ -0,0 +1,27 @@
+package hk.edu20250717.day11;
+
+public class D1_CustomerMain {
+	public static void main(String[] args) {
+		D1_Customer customerLee=new D1_Customer(); //부모의 타입으로 부모 생성함
+		customerLee.customerID=10001;
+		customerLee.customerName="이순신";
+		customerLee.bonusPoint=1000;
+		customerLee.calcPrice(15000);
+		System.out.println(customerLee.toString());
+		
+		//부모의 타입으로 부모 생성함
+		D1_Customer customerLee2=new D1_Customer(10001,"이순신"); 
+		customerLee.bonusPoint=1000;
+		customerLee.calcPrice(15000);
+		System.out.println(customerLee2.toString()+price2);
+		
+		//자식의 타입으로 자식을 생성
+		D1_VIPCustomer customerKim=new D1_VIPCustomer(10002,"김유신",20001);
+		customerKim.bonusPoint=10000;
+		System.out.println(customerKim.toString()+price3);
+		
+
+	}
+	
+	
+}
---
+++ hk/edu20250717/day11/D2_Tiger.java
@@ -0,0 +1,11 @@
+package hk.edu20250717.day11;
+
+public class D2_Tiger extends D2_Animal{
+	@Override
+	public void move() {
+		System.out.println("호랑이가 네 발로 뜁니다.");
+	}
+	public void eat() {
+		System.out.println("호랑이가 찹쌀떡을 먹고 있어요.");
+	}
+}
