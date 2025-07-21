f78215422ab84551973b65498d9acfdb8a7d35de
01lcw <163083611+01lcw@users.noreply.github.com>
2025-07-21 17:34:28 +0900

    Add files via upload

---
+++ sp/D6/D5_EarthMain.java
@@ -0,0 +1,8 @@
+package sp.D6;
+
+public class D5_EarthMain {
+	public static void main(String[] args) {
+		System.out.println("반지름"+D5_Earth.EARTH_RADIUS+"km");
+		System.out.println("표면적"+D5_Earth.EARTH_SURFACE_AREA+"km");
+	}
+}
